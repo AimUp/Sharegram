0b81845304a0b44083660c7c6778c49f0d7b23fc
Aimar Ugarte <ugarteaimar@gmail.com>
2017-11-29 12:13:14 +0100

    Hasiera

---
+++ signUp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Sharegram: Sign in.</title>
+		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+	</head>
+	<body>
+		<h1>Sharegram: Sign in</h1>
+		<h2>Ongi etorri Sharegramera, igo zure argazkiak eta besteenak ...</h2>
+		<div class="signin">
+			<form method=”POST” id="login" action="">
+			Hasi saioa:
+			<input type=”text” name="izena"/>
+			<input type=”text” name=”erabiltzailea”/>
+			<input type="password" name="pasahitza"/>
+			<input type="submit" value="Log in"/>
+			</form>
+		</div>
+	</body>
+</html>
