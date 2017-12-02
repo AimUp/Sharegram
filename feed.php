a32a0a0b955b8e41c45ff554723d85806da5a0fb
hvadillo <vadillo.hector@gmail.com>
2017-12-02 20:29:26 +0100

    feed-aren hasiera

--- feed.php
+++ feed.php
@@ -2,3 +2,14 @@
 	$title = "Sharegram";
 	include "estruktura.php" 
 ?>
+	<div class="feed">
+		<?php
+			for(i=1; /*i<=irudi kopurua karpetan*/; i++){
+				$irudia=./irudi1;
+				//pisaria la anterior imagen o haria una tabla de imagenes en bertical??
+				echo "<tr>"
+				echo "<img src="$irudia"/>"
+				echo "</tr>"
+			}
+		?>
+	</div>
