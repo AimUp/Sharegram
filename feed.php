6c3e8f6416a92e4088cacbee102ae4e3aa9129c7
hvadillo <vadillo.hector@gmail.com>
2017-12-03 13:21:33 +0100

    argazkiak igotzeko lehenengo pausoak

--- feed.php
+++ feed.php
@@ -2,14 +2,33 @@
 	$title = "Sharegram";
 	include "estruktura.php" 
 ?>
-	<div class="feed">
-		<?php
-			for(i=1; /*i<=irudi kopurua karpetan*/; i++){
-				$irudia=./irudi1;
-				//pisaria la anterior imagen o haria una tabla de imagenes en bertical??
-				echo "<tr>"
-				echo "<img src="$irudia"/>"
-				echo "</tr>"
-			}
-		?>
-	</div>
+	<body>
+		<div class="feed">
+			<?php
+				for(i=1; /*i<=irudi kopurua karpetan*/; i++){
+					$irudia=./i;
+					//pisaria la anterior imagen o haria una tabla de imagenes en bertical??
+					echo "<tr>"
+					echo "<img src="$irudia"/>"
+					echo "</tr>"
+				}
+			?>
+		</div>
+			<form method="post">
+    			<input type="submit" name="igo" id="igo" value="Argazkia igo" />
+			</form>
+			<?php
+				function argazkiaIgo(){
+   					//argazkiak igotzeko lekua
+   					echo "<form method="post"/>";
+   					echo "<input type="submit" name="argazkiigo" id="argazkibotoia" value="Igo"/>";
+   					echo "<form/>";
+				}
+
+				if(array_key_exists('igo',$_POST)){
+   					argazkiaIgo();
+				}
+			?>
+		</div>
+	</body>
+</html>
